cda9625cba234ef72c1b6e39e06555a3a9d80435
Michael Williams <michael.williams@enspiral.com>
2019-06-14 00:41:30 +1200

    chore: fix metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,10 +2,9 @@ const path = require('path')
 
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Starter MDX Basic',
-    description:
-      'Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.',
-    author: '@chrisbiscardi'
+    title: "@ahdinosaur' Gatsby starter",
+    description: 'Cheers to your next Gatsby project',
+    author: '@ahdinosaur'
   },
   plugins: [
     {
