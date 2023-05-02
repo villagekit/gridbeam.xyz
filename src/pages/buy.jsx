d260f5eee4a6a3453b05b0e98d4446fca8add901
Michael Williams <michael.williams@enspiral.com>
2023-05-02 17:38:04 +1200

    simplify buy page

--- src/pages/buy.jsx
+++ src/pages/buy.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Box, Flex, Text, Link, Button } from 'rebass/styled-components'
+import { Box, Flex, Text, Link, Image } from 'rebass/styled-components'
 import shader from 'shader'
 
 import Layout from '../components/layout'
@@ -10,51 +10,14 @@ import background from '../helpers/background'
 
 const suppliers = [
   {
-    name: 'GridBeam.com',
+    name: 'Original Grid Beam',
     href: 'https://web.archive.org/web/20200226012424/http://www.gridbeam.com/',
     isDead: true,
-    location: 'California, USA',
-    currency: 'USD',
-    systemOfMeasurement: 'imperial',
-    products: [
-      {
-        width: '1.5 inches',
-        holeDiameter: '5/16 inch',
-        material: 'wood',
-        price: '$3.00 - $3.50 per foot',
-        href: 'https://web.archive.org/web/20200226012733/http://www.gridbeam.com/woodproducts.html'
-      },
-      {
-        width: '1.5, 2, or 3 inches',
-        holeDiameter: '13/32, 7/16, or 3/4 inch',
-        material: 'aluminum',
-        price: '$7.00, $9.00, or $10.00 per foot',
-        href: 'https://web.archive.org/web/20200226012726/http://www.gridbeam.com/metalproducts.html'
-      },
-      {
-        width: '2 inches',
-        holeDiameter: '7/16 inch',
-        material: 'steel',
-        price: '$5.00 per foot',
-        href: 'https://web.archive.org/web/20200226012726/http://www.gridbeam.com/metalproducts.html'
-      }
-    ]
   },
   {
-    name: 'GridKit.nz',
-    href: 'https://gridkit.nz',
-    location: 'New Zealand',
-    currency: 'NZD',
-    systemOfMeasurement: 'metric',
-    products: [
-      {
-        width: '40 mm',
-        holeDiameter: '8 mm',
-        material: 'wood',
-        price: '$12 - $15 per meter',
-        href: 'https://gridkit.nz/order'
-      }
-    ]
+    name: 'Grid Kit',
+    href: 'https://gridkit.nz/order',
+    logo: '/suppliers/gridkit.svg'
   }
 ]
 
@@ -125,10 +88,7 @@ function Supplier (props) {
     name,
     href,
     isDead,
-    location,
-    currency,
-    systemOfMeasurement,
-    products
+    logo,
   } = props
 
   return (
@@ -141,131 +101,24 @@ function Supplier (props) {
           textDecoration: 'none'
         }}
       >
-        <Text as='h3' fontSize={4} fontFamily='heading'>
+        <Text as='h3' fontSize={4} fontFamily='heading' display='flex' alignItems='center'>
           <Box
             css={{
               display: 'inline',
-              textDecoration: isDead ? 'line-through' : 'default'
+              textDecoration: isDead ? 'line-through' : 'default',
+              marginRight: '1rem',
             }}
           >
             {name}
           </Box>
           {isDead && <>
-            {' '}
             <span aria-label='dead' role='img'>💀</span>
+          </> ||
+          logo != null && <>
+            <Image width="auto" height="36px" src={logo} />
           </>}
         </Text>
       </Link>
-      <Box
-        as='dl'
-        my={3}
-        mx={[1, 2, 3]}
-        sx={{
-          dt: {
-            fontSize: 1,
-            marginBottom: 2
-          },
-          dd: {
-            fontSize: 1
-          }
-        }}
-        css={{
-          dt: {
-            display: 'inline-block',
-            width: '50%',
-            fontFamily: 'heading',
-            fontWeight: 'bold'
-          },
-          'dd:not(:nth-child(8))': {
-            display: 'inline-block',
-            width: '50%'
-          }
-        }}
-      >
-        <dt>Location</dt>
-        <dd>{location}</dd>
-
-        <dt>Currency</dt>
-        <dd>{currency}</dd>
-
-        <dt>System of measurement</dt>
-        <dd>{systemOfMeasurement}</dd>
-
-        <dt>Products</dt>
-        <dd>
-          <Products products={products} />
-        </dd>
-      </Box>
-    </Box>
-  )
-}
-
-function Products (props) {
-  const { products } = props
-
-  return (
-    <Box
-      as='table'
-      sx={{
-        tr: {
-          paddingTop: 3,
-          paddingBottom: 3
-        },
-        th: {
-          fontFamily: 'heading',
-          fontWeight: 'bold'
-        },
-        'td, th': {
-          fontSize: 0,
-          paddingLeft: 1,
-          paddingRight: 1
-        },
-        'td:first-child': {
-          paddingLeft: 0
-        }
-      }}
-      css={{
-        'th:nth-child(1), td:nth-child(1)': {
-          width: '8%'
-        },
-        'th:nth-child(2), td:nth-child(2)': {
-          width: '10%'
-        },
-        'th:nth-child(3), td:nth-child(3)': {
-          width: '27%'
-        },
-        'th:nth-child(4), td:nth-child(4)': {
-          width: '20%'
-        },
-        'th:nth-child(5), td:nth-child(5)': {
-          width: '25%'
-        }
-      }}
-    >
-      <tr>
-        <th />
-        <th>Material</th>
-        <th>Price</th>
-        <th>Width</th>
-        <th>Hole Diameter</th>
-      </tr>
-      {products.map(product => {
-        const { width, holeDiameter, material, price, href } = product
-
-        return (
-          <tr>
-            <td>
-              <Button as={Link} href={href} target='_blank' rel='noopener' my={2}>
-                Buy
-              </Button>
-            </td>
-            <td>{material}</td>
-            <td>{price}</td>
-            <td>{width}</td>
-            <td>{holeDiameter}</td>
-          </tr>
-        )
-      })}
     </Box>
   )
 }
