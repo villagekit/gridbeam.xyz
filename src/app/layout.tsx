544e6eac5f414993d6ee51ef79f4658956cb5c21
Michael Williams <hello@mikey.nz>
2025-06-09 13:43:15 +1200

    add Chakra

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+
+import { Provider as ChakraProvider } from "@/components/ui/provider"
+
 import "./globals.css";
 
 const geistSans = Geist({
@@ -23,9 +26,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+        <ChakraProvider>
+          {children}
+        </ChakraProvider>
       </body>
     </html>
   );
